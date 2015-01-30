344f4f4d6cee1900cacb1c7f3e2060b6dc88ebe8
Christian Vanderwall <cvanderwall14@gmail.com>
2015-01-30 21:07:02 +0000

    Chapter: Sessions

--- leaderboard.js
+++ leaderboard.js
@@ -6,14 +6,19 @@ if(Meteor.isClient){
     'player': function(){
       return PlayersList.find();
     },
-    'otherHelperFunction': function(){
-      return "Some other function.";
+    'selectedClass': function(){
+      var playerId = this._id;
+      var selectedPlayer = Session.get('selectedPlayer');
+      if(playerId === selectedPlayer){
+        return "selected"
+      }
     }
   });
   
   Template.leaderboard.events({
     'click .player': function() {
-      console.log("Clicked a player!");
+      var playerId = this._id;
+      Session.set('selectedPlayer', playerId);
     }
   });
   
