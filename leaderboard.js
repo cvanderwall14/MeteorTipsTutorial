2209d52a8459b911d76965cc39bd6e391eef5ed0
Christian Vanderwall <cvanderwall14@gmail.com>
2015-01-30 20:44:47 +0000

    Chapter: Templates

--- leaderboard.js
+++ leaderboard.js
@@ -1,1 +1,18 @@
 PlayersList = new Mongo.Collection('players');
+
+if(Meteor.isClient){
+  
+  Template.leaderboard.helpers({
+    'player': function(){
+      return PlayersList.find();
+    },
+    'otherHelperFunction': function(){
+      return "Some other function.";
+    }
+  });
+  
+}
+
+if(Meteor.isServer){
+  
+}
